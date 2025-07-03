81522655114dacf7f13f5bf26610ccfd0c79cc07
tazaa01 <136049637+tazaa01@users.noreply.github.com>
2025-07-03 15:01:07 +0700

    Add files via upload

---
+++ itthimon/พื้นฐานตัวแปรและชนิดข้อมูล.py
@@ -0,0 +1,15 @@
+name = "อิทธิมนต์"
+age = 18
+height = 1.69
+is_student = True
+print("ชื่่อ:", name)
+print("อายุ:", age)
+print("สวนสูง:",height)
+print("สถานะนักเรียน:", is_student)
+
+print("ชนิดของ",type(name))
+print("ชนิดของ",type(age))
+print("ชนิดของ",type(height))
+print("ชนิดของ",type(is_student))
+
+print("สวัสดี" + name + "อายุ" + str(age) + "ปี")
